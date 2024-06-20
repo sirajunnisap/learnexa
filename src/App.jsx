b320568b18704a36b25db74450cb4b19959f96b9
sirajunnisa <sirajunnisap@gmail.com>
2024-06-20 17:17:57 +0530

    slider change

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,48 @@
-import { useEffect, useState } from 'react';
 import Navbar from "./components/navbar/Navbar"
 import './App.scss'
-// import Arabic from './components/arabic/Arabic'
+import Arabic from './components/arabic/Arabic'
 import Hero from "./components/hero/Hero"
 import Contact from "./components/contact/Contact"
-// import Careers from "./components/careers/Careers"
+import Careers from "./components/careers/Careers"
 import Footer from "./components/footer/Footer"
 import About from "./components/about/About"
-// import Testimonials from "./components/testimonials/Testimonials"
+import Testimonials from "./components/testimonials/Testimonials"
 
 function App() {
-  const [testimonialsHTML, setTestimonialsHTML] = useState('');
-
-  useEffect(() => {
-    fetch('/testimonials.html')
-      .then(response => response.text())
-      .then(html => setTestimonialsHTML(html))
-      .catch(error => console.error('Error fetching the testimonials HTML:', error));
-  }, []);
-
+  
   return (
+    <>
     <div>
       <section id="Homepage">
-        <Navbar />
-        <Hero />
+        <Navbar/>
+        <Hero/>
       </section>
-
-      <section id="About">
-        <About />
+    
+    <section id="About">
+      <About/>
       </section>
+  
 
-      {/* <Careers /> */}
+      
 
-      <section id="Testimonials">
-        <div dangerouslySetInnerHTML={{ __html: testimonialsHTML }} />
+        {/* <Careers /> */}
+
+       <section id="Testimonials">
+        <Testimonials/>
       </section>
-      {/* <section id="Testimonials">
-        <Testimonials />
-      </section> */}
       <section id="Contact">
         <Contact />
-        <Footer />
+        <Footer/>
       </section>
+{/* <section>
+  <Arabic/>
+</section> */}
       
-      {/* <section>
-        <Arabic />
-      </section> */}
+        
+      
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,116 +1,12 @@
 
-// import Swiper from "swiper";
-// import './testimonials.scss';
+
+import './testimonials.scss';
 // import 'swiper/swiper-bundle.min.css';
 
-// const Testimonials = () => {
-  
-  
-//   const swiper = new Swiper('.swiper', {
-//     autoHeight: true,
-//     loop: true,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-   
-//   });
-
-//   return (
-//     <div className="testi-section">
-//       <div className="site">
-//         <div className="container">
-//           <div className="testi">
-//             <div className="head">
-//               <h3>Testimonials</h3>
-//               <p>See what people are saying.</p>
-//             </div>
-//             <div className="body swiper">
-
-//              <ul className="swiper-wrapper">
-//              <li className="swiper-slide">
-//                   <div className="testiwrapper">
-//                     <div className="thumbnail">
-//                       <img src="/hero.png" alt="" />
-//                     </div>
-//                     <div className="aside">
-//                       <p>
-//                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//                         Vel, voluptates natus nobis incidunt omnis ad at et
-//                         optio. Similique ducimus iste fuga aliquam in cum
-//                         corrupti possimus neque laborum eius.
-//                       </p>
-//                       <div className="name">
-//                         <h4>Neha</h4>
-//                         <p>Youtube Influencer</p>
-//                       </div>
-//                     </div>
-//                   </div>
-
-//                   {/* <div className="testiwrapper">
-//                     <div className="thumbnail2">
-//                       <img src="/hero.png" alt="" />
-//                     </div>
-//                     <div className="aside">
-//                       <p>
-//                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//                         Vel, voluptates natus nobis incidunt omnis ad at et
-//                         optio. Similique ducimus iste fuga aliquam in cum
-//                         corrupti possimus neque laborum eius.
-//                       </p>
-//                       <div className="name">
-//                         <h4>Neha</h4>
-//                         <p>Youtube Influencer</p>
-//                       </div>
-//                     </div>
-//                   </div> */}
-//                  </li>
-//               </ul>
-
-//               <div className="swiper-pagination"></div>
-
-//               <div className="swiper-button-prev"></div>
-//               <div className="swiper-button-next"></div>
-              
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Testimonials;
-import React, { useEffect } from 'react';
-import './testimonials.scss'; // Ensure your custom CSS/SCSS file is correctly named
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-
-import 'swiper/swiper-bundle.min.css';
-
 const Testimonials = () => {
-  useEffect(() => {
-    new Swiper('.swiper', {
-      modules: [Pagination, Navigation],
-      autoHeight: true,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  }, []);
+  
+  
+  
 
   return (
     <div className="testi-section">
@@ -121,27 +17,49 @@ const Testimonials = () => {
               <h3>Testimonials</h3>
               <p>See what people are saying.</p>
             </div>
-            <div className="body swiper">
-              <ul className="swiper-wrapper">
-                <li className="swiper-slide">
+            <div className="body ">
+
+             <ul className="">
+             <li className="">
                   <div className="testiwrapper">
                     <div className="thumbnail">
-                      <img src="hero.png" alt="Thumbnail" />
+                      <img src="/hero.png" alt="" />
                     </div>
                     <div className="aside">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, voluptates natus nobis incidunt omnis ad at et optio. Similique ducimus iste fuga aliquam in cum corrupti possimus neque laborum eius.</p>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vel, voluptates natus nobis incidunt omnis ad at et
+                        optio. Similique ducimus iste fuga aliquam in cum
+                        corrupti possimus neque laborum eius.
+                      </p>
                       <div className="name">
                         <h4>Neha</h4>
-                        <p>YouTube Influencer</p>
+                        <p>Youtube Influencer</p>
                       </div>
                     </div>
                   </div>
-                </li>
-                {/* Repeat similar structure for other testimonials */}
+
+                  {/* <div className="testiwrapper">
+                    <div className="thumbnail2">
+                      <img src="/hero.png" alt="" />
+                    </div>
+                    <div className="aside">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Vel, voluptates natus nobis incidunt omnis ad at et
+                        optio. Similique ducimus iste fuga aliquam in cum
+                        corrupti possimus neque laborum eius.
+                      </p>
+                      <div className="name">
+                        <h4>Neha</h4>
+                        <p>Youtube Influencer</p>
+                      </div>
+                    </div>
+                  </div> */}
+                 </li>
               </ul>
-              <div className="swiper-pagination"></div>
-              <div className="swiper-button-prev"></div>
-              <div className="swiper-button-next"></div>
+
+             
             </div>
           </div>
         </div>
